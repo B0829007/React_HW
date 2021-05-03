69fd180a905d73d5f6561af1de6e1351e0130ba6
B0829007 <a0927373726@gmail.com>
2021-05-03 21:09:28 +0800

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SignInSide from './signin'
+
+function App() {
+  return (
+    <div className="App">
+      <div>{SignInSide()}</div>
+    </div>
+  );
+}
+
+export default App;
